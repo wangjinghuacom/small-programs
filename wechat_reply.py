1ea0c64b1f6246e2bbe5fb2ae10f097a642c357d
Six <guzhizi123@163.com>
2019-05-15 22:05:35 +0800

    修改逻辑错误

--- wechat_reply.py
+++ wechat_reply.py
@@ -5,6 +5,7 @@
 基于itchat的微信自动回复设置，非工作时间处理公务是真的烦，所以写个程序来背锅。
 
 author: zhuzi  date: 2019/04/20    version: 1.0
+author: zhuzi  date: 2019/05/03    version: 1.1     function: 增加了图灵机器人
 """
 
 import itchat
@@ -12,7 +13,6 @@ import time
 import random
 import requests
 import json
-
 
 # 对群消息进行自动回复
 @itchat.msg_register('Text', isGroupChat = True)
@@ -22,40 +22,40 @@ def group_reply(msg):
     print('%s %s：%s' % (timestr, msg.actualNickName, msg.text))
     # 是否有人@自己
     if msg.isAt:
-        time.sleep(random.random())
+        time.sleep(random.uniform(0, 3))
         itchat.send('%s %s：\n“%s”' % (timestr, msg.actualNickName, msg.text), toUserName = 'filehelper')
-        time.sleep(random.random())
-        return (u'[自动回复] %s\n　　您好，我是助手Diana，主人五一不在线，您的消息已收到，我会及时转达。\n【消息以"@Diana $"开头，我可以尝试进行回复】' % timestr)
-    # 以'@Diana $'开头的文本调用图灵机器人进行回复
-    if msg.text.startswith('@Diana $'):
-        robot_ans = tuling_reply(msg.text.lstrip('@Diana $'))
-        time.sleep(random.random())
+        time.sleep(random.uniform(0, 3))
+        return (u'[自动回复] %s\n　　您好，我是助手Diana，主人周末不上线，您的消息已收到，我会及时转达。\n【消息以"$"开头，我可以尝试进行回复】' % timestr)
+    # 以'$'开头的文本调用图灵机器人进行回复
+    if msg.text.startswith('$') or msg.text.startswith('＄'):
+        robot_ans = tuling_reply(msg.text.lstrip('$＄'))
+        time.sleep(random.uniform(0, 3))
         return robot_ans
-
 
 # 对文本、图片、语音、视频、分享、附件内容进行自动回复
 @itchat.msg_register(['Text', 'Picture', 'Recording', 'Video', 'Sharing', 'Attachment'])
 def content_reply(msg):
 
+    timestr = time.strftime("%m-%d %H:%M:%S", time.localtime())
     # 所有文本消息都给文件传输助手转发一份
     if msg['Type'] == 'Text':
         # 以'$'开头的文本调用图灵机器人进行回复
-        if msg.text.startswith('$'):
-            robot_ans = tuling_reply(msg.text.lstrip('$'))
-            time.sleep(random.random())
+        if msg.text.startswith('$') or msg.text.startswith('＄'):
+            print('%s %s：“%s”' % (timestr, msg.user.RemarkName, msg.text))
+            #print('%s %s：“%s”' % (timestr, msg['User']['RemarkName'], msg['Content']))
+            robot_ans = tuling_reply(msg.text.lstrip('$＄'))
+            time.sleep(random.uniform(0, 3))
             return robot_ans
-        timestr = time.strftime("%m-%d %H:%M:%S", time.localtime())
         # 将不是自己发送的消息转发给文件助手
         if not msg.fromUserName == myUserName:
             print('%s %s：“%s”' % (timestr, msg.user.RemarkName, msg.text))
-            #print('%s %s：“%s”' % (timestr, msg['User']['RemarkName'], msg['Content']))
-            time.sleep(random.random())
+            time.sleep(random.uniform(0, 3))
             itchat.send('%s %s：\n“%s”' % (timestr, msg.user.RemarkName, msg.text), toUserName = 'filehelper')
         else:
             print('%s %s：“%s”' % (timestr, '自己', msg.text))
             
-    time.sleep(random.random())
-    return (u'[自动回复] %s\n　　您好，我是助手Diana~，主人五一不在线哦，小的会如实转达您的留言~\n【试试以"$"开头进行回复，我会尽量尝试满足您的需求】' % timestr)
+    time.sleep(random.uniform(0, 3))
+    return (u'[自动回复] %s\n　　您好，我是助手Diana~，主人正忙，小的会如实转达您的留言~\n【试试以"$"开头进行回复，我会尽量尝试满足您的需求】' % timestr)
 
 
 def tuling_reply(text):
@@ -76,7 +76,10 @@ def tuling_reply(text):
         }
     }
     r = requests.post(apiurl, data = json.dumps(data)).json()
-    return r['results'][0]['values']['text']
+    try:
+        return r['results'][0]['values']['text']
+    except KeyError:
+        return r['results'][1]['values']['text'] + '\n' + r['results'][0]['values']['url']
 
 
 if __name__ == '__main__':
